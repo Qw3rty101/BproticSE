8836dac007de9a2928997f5524de123123d799ad
Siraj Nurul Bil Haq <siraj.nurul@gmail.com>
2023-12-02 12:18:57 +0700

    pertemuan4

---
+++ pertemuan4/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    message : 'Success',
+    data : {
+      Text : 'Hello API',
+      myPort : `${port}`,
+      params: req.param('coba')
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port: http://localhost:${port}`)
+})
